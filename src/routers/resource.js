c34f0eaaf7b962d2a8820d9a713cc2a46fddab6f
Duc Nam <namnd0210@gmail.com>
2020-11-08 09:45:58 +0700

    navigate after login

--- src/routers/resource.js
+++ src/routers/resource.js
@@ -1,8 +1,40 @@
 import React from 'react';
-import { route } from 'navi';
+import { route, withView, mount } from 'navi';
+import { View } from 'react-navi';
 
 import Login from 'containers/Login';
+import withRoles from './withRoles';
+
+import AdminHomepage from 'role/admin/containers/Homepage';
+import GuiderHomepage from 'role/guider/containers/Homepage';
+import UserHomepage from 'role/user/containers/Homepage';
 
 export const routes = {
   '/login': ('/', route({ title: 'Login', view: <Login /> })),
+  '/admin': withRoles(
+    'admin',
+    '/admin',
+    withView(
+      <View />,
+      mount({
+        '/homepage': route({ title: 'Admin Homepage', view: <AdminHomepage /> }),
+      }),
+    ),
+  ),
+  '/guider': withRoles(
+    'guider',
+    '/guider',
+    withView(
+      <View />,
+      mount({
+        '/homepage': route({ title: 'Guider Homepage', view: <GuiderHomepage /> }),
+      }),
+    ),
+  ),
+  '/user': withView(
+    <View />,
+    mount({
+      '/homepage': route({ title: 'User Homepage', view: <UserHomepage /> }),
+    }),
+  ),
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,6 @@
 export const CURRENT_USER = 'currentUser';
 export const TOKEN = 'token';
+
+export const ADMIN = 'admin';
+export const USER = 'user';
+export const GUIDER = 'guider';
